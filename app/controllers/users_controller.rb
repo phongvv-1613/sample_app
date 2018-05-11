9acbf085b02465a57b735d17c4410f93942f4f78
= <vuphong95663@gmail.com>
2018-05-11 14:35:05 +0700

    Finish chapter_9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by id: params[:id]
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = t(".success")
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
   end
 
